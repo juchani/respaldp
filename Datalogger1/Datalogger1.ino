b354be080fecbf77f9442a3d5f3981147e8592ae
juchani <victorfrancojuchani@gmail.com>
2020-03-06 10:46:37 -0400

    Add files via upload

---
+++ Datalogger1/Datalogger1.ino
@@ -0,0 +1,51 @@
+#include <SPI.h>
+#include <SD.h>
+const int chipSelect = 4;
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+}
+
+void loop() {
+  // make a string for assembling the data to log:
+  String dataString = "";
+
+  // read three sensors and append to the string:
+  for (int analogPin = 0; analogPin < 3; analogPin++) {
+    int sensor = analogRead(analogPin);
+    dataString += String(sensor);
+    if (analogPin < 2) {
+      dataString += ",";
+    }
+  }
+  File dataFile = SD.open("datalog.cvs", FILE_WRITE);
+  if (dataFile) {
+    dataFile.println(dataString);
+    dataFile.close();
+    Serial.println(dataString);
+  }
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+}
+
+
+
+
+
+
+
+
+
