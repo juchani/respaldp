6c562261385af417f62d71bf34072454d85ad199
juchani <victorfrancojuchani@gmail.com>
2020-03-07 04:54:00 -0400

    resp lib

---
+++ libraries/SparkFun_Haptic_Motor_Driver/README.md
@@ -0,0 +1,40 @@
+SparkFun DRV2605L Arduino Library
+========================================
+
+![SparkFun DRV2605L Haptic Motor Driver](https://cdn.sparkfun.com/assets/parts/1/1/8/4/7/14031-01.jpg)
+
+[*SparkFun Haptic Motor Driver (ROB-14031)*](https://www.sparkfun.com/products/14031)
+
+Arduino library for the DRV2605L.
+
+Repository Contents
+-------------------
+
+* **/examples** - Example sketches for the library (.ino). Run these from the Arduino IDE. 
+* **/src** - Source files for the library (.cpp, .h).
+* **library.properties** - General library properties for the Arduino package manager. 
+
+Documentation
+--------------
+
+* **[Installing an Arduino Library Guide](https://learn.sparkfun.com/tutorials/installing-an-arduino-library)** - Basic information on how to install an Arduino library.
+* **[Product Repository](https://github.com/sparkfun/Haptic_Motor_Driver)** - Main repository (including hardware files) for the Haptic Motor Driver.
+* **[Hookup Guide](https://learn.sparkfun.com/tutorials/haptic-motor-driver-hook-up-guide** - Basic hookup guide for the Haptic Motor Driver.
+
+Products that use this Library 
+---------------------------------
+
+* [ROB-14031](https://www.sparkfun.com/products/14031?_ga=1.59525741.1788647015.1453914745)- Haptic Motor Driver
+
+License Information
+-------------------
+
+This product is _**open source**_! 
+
+Please review the LICENSE.md file for license information. 
+
+If you have any questions or concerns on licensing, please contact techsupport@sparkfun.com.
+
+Distributed as-is; no warranty is given.
+
+- Your friends at SparkFun.
---
+++ libraries/TelegramBot/TelegramBot.cpp
@@ -0,0 +1,136 @@
+
+// Copyright Casa Jasmina 2016
+// LGPL License
+//
+// TelegramBot library
+// https://github.com/CasaJasmina/TelegramBot-Library
+
+#include "TelegramBot.h"
+
+TelegramBot::TelegramBot(const char* token, const char* name, const char* username, Client &client)	{
+	this->client = &client;
+	this->token=token;
+	this->name=name;
+	this->username=username;
+}
+
+void TelegramBot::begin()	{
+	while(!client->connected()){
+		client->connect(HOST, SSL_PORT);
+		delay (2000);
+	}
+}
+
+/************************************************************************************
+ * GetUpdates - function to receive messages from telegram as a Json and parse them *
+ ************************************************************************************/
+message TelegramBot::getUpdates()  {
+		begin();
+
+		//Send your request to api.telegram.org
+		String getRequest = "GET /bot"+String(token)+"/getUpdates?offset="+String(last_message_recived)+" HTTP/1.1";
+		client->println(getRequest);
+		client->println("User-Agent: curl/7.37.1");
+		client->println("Host: api.telegram.org");
+		client->println("Accept: */*");
+		client->println();
+
+		String payload = readPayload();
+	    if (payload != "") {
+			message m;
+			StaticJsonBuffer<JSON_BUFF_SIZE> jsonBuffer;
+			JsonObject & root = jsonBuffer.parseObject(payload);
+			int update_id = root["result"][1]["update_id"];
+			if(last_message_recived != update_id){
+				String sender = root["result"][1]["message"]["from"]["username"];
+				String text = root["result"][1]["message"]["text"];
+				m.chat_id = root["result"][1]["message"]["chat"]["id"];
+				String date = root["result"][1]["message"]["date"];
+
+				m.sender = sender;
+				m.text = text;
+				m.date = date;
+				last_message_recived=update_id;
+				return m;
+			}else{
+				m.chat_id = 0;
+				return m;
+			}
+		}
+	}
+
+// send message function
+// send a simple text message to a telegram char
+String TelegramBot::sendMessage(int chat_id, String text)  {
+		StaticJsonBuffer<JSON_BUFF_SIZE> jsonBuffer;
+		JsonObject& buff = jsonBuffer.createObject();
+		buff["chat_id"] = chat_id;
+		buff["text"] = text;
+
+		String msg;
+		buff.printTo(msg);
+		return postMessage(msg);
+	}
+
+// send a message to a telegram chat with a reply markup
+String TelegramBot::sendMessage(int chat_id, String text, TelegramKeyboard &keyboard_markup, bool one_time_keyboard, bool resize_keyboard)  {
+		StaticJsonBuffer<JSON_BUFF_SIZE> jsonBuffer;
+		JsonObject& buff = jsonBuffer.createObject();
+		buff["chat_id"] = chat_id;
+		buff["text"] = text;
+
+		JsonObject& reply_markup = buff.createNestedObject("reply_markup");
+		JsonArray& keyboard = reply_markup.createNestedArray("keyboard");
+
+		for (int a = 1 ; a <= keyboard_markup.length() ; a++){
+			JsonArray& row = keyboard.createNestedArray();
+				for( int b = 1; b <= keyboard_markup.rowSize(a) ; b++){
+					row.add(keyboard_markup.getButton(a,b));
+					Serial.println(b);
+				}
+		}
+
+		reply_markup.set<bool>("one_time_keyboard", one_time_keyboard);
+		reply_markup.set<bool>("resize_keyboard", resize_keyboard);
+		reply_markup.set<bool>("selective", false);
+
+		String msg;
+		buff.printTo(msg);
+		// Serial.println(msg);
+		return postMessage(msg);
+}
+
+// gets the telegram json string
+// posts the message to telegram
+// returns the payload
+String TelegramBot::postMessage(String msg) {
+		begin();
+
+		client->println("POST /bot"+String(token)+"/sendMessage"+" HTTP/1.1");
+		client->println("Host: api.telegram.org");
+	    client->println("Content-Type: application/json");
+	    client->println("Connection: close");
+	    client->print("Content-Length: ");
+	    client->println(msg.length());
+	    client->println();
+	    client->println(msg);
+
+		return readPayload();
+}
+
+// reads the payload coming from telegram server
+// returns the payload string
+String TelegramBot::readPayload(){
+	char c;
+	String payload="";
+		//Read the answer and save it in String payload
+		while (client->connected()) {
+		payload = client->readStringUntil('\n');
+		if (payload == "\r") {
+			break;
+		 }
+	  }
+	payload = client->readStringUntil('\r');
+	// Serial.println(payload);
+	return payload;
+}
